2abb843024b853f92d09c5a74c8da995f22db133
yooonyng <yooonyng@gmail.com>
2022-06-22 12:43:48 +0900

    0622

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ from flask import Flask
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
 from config import Config
+from resource.follow import FollowListResource, FollowResource
 from resource.memo import MemoInfoResource, MemoListResource
 from resource.user import UserLoginResource, UserLogoutResource, UserRegisterResource, jwt_blacklist
 
@@ -33,6 +34,8 @@ api.add_resource(UserLoginResource,'/users/login')
 api.add_resource(UserLogoutResource,'/users/logout')
 api.add_resource(MemoListResource,'/memo')
 api.add_resource(MemoInfoResource,'/memo/<int:memo_id>')
+api.add_resource(FollowResource,'/follow/<int:follow_id>')
+api.add_resource(FollowListResource,'/follow')
 
 if __name__ == '__main__':
     app.run()
